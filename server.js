ea98f0cfb61311e46e274503b45feb75ef9c14b9
Daniel Masters <daniel.p.masters@gmail.com>
2019-10-26 12:36:06 -0500

    intial file creation and package install

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "newsScraper";
+var collections = ["scrapedNews"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
